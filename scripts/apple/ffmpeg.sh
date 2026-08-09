e0f915cf85735c8e58a26737b921411611b225cd
기 <gi@giui-MacBookAir.local>
2026-08-10 00:15:43 +0900

    fix: capture FFmpeg ABI-major dylib symbols

--- scripts/apple/ffmpeg.sh
+++ scripts/apple/ffmpeg.sh
@@ -700,7 +700,7 @@ build_ffmpeg
 if [[ -n ${FFMPEG_KIT_DSYM_BUILD:-} ]]; then
   for FFMPEG_LIB in "${FFMPEG_LIBS[@]}"; do
     "${BASEDIR}/tools/apple/capture-thin-dsym.sh" \
-      "${BASEDIR}/src/ffmpeg/${FFMPEG_LIB}/${FFMPEG_LIB}.$(get_ffmpeg_library_version "${FFMPEG_LIB}").dylib" \
+      "${BASEDIR}/src/ffmpeg/${FFMPEG_LIB}/${FFMPEG_LIB}.$(get_ffmpeg_library_major_version "${FFMPEG_LIB}").dylib" \
       "${BASEDIR}/prebuilt/apple-thin-dsyms/${FULL_ARCH}/${FFMPEG_LIB}.dSYM" \
       1>>"${BASEDIR}"/build.log 2>&1 || exit 1
   done
